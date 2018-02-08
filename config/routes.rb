9019ad3ae6c8a95750f90e786e6f05f23a87b44b
Al Kevin Tan <alkevintan@gmail.com>
2018-02-08 15:09:49 +0800

    init

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'students/index'
+
+  get 'students/show'
+
+  get 'students/new'
+
+  get 'students/create'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
